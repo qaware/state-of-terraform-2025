5231b1e51d08db18b982d6e8742a15dc5ffecfa2
Florian Wende <extern.wende_florian@allianz.de>
2025-02-06 19:21:32 +0100

    Add variable validation

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 TODO
 
+<!-- TOC -->
+* [state-of-terraform-2025](#state-of-terraform-2025)
+  * [How to demo](#how-to-demo)
+    * [1. Basic setup](#1-basic-setup)
+    * [2. Validations](#2-validations)
+<!-- TOC -->
+
 ## How to demo
 
-### Basic setup
+### 1. Basic setup
 
 1. Start LocalStack Container
 
@@ -26,3 +33,19 @@ terraform apply
 
 4. Open the Static Website in Browser
    The link will be displayed in the console output of `terraform apply` and will look something like [ `https://some-example-bucket-for-a-static-website.s3-website.localhost.localstack.cloud:4566/`](https://some-example-bucket-for-a-static-website.s3-website.localhost.localstack.cloud:4566/).
+
+
+### 2. Validations
+
+1. Validation fails
+
+```bash
+terraform plan --var bucket_name="1.1.1.1"
+```
+
+2. Validation passes
+
+```bash
+terraform plan --var bucket_name="some-example-bucket-for-a-static-website"
+```
+
--- variables.tf
+++ variables.tf
@@ -2,4 +2,25 @@ variable "bucket_name" {
     description = "The name of the S3 bucket to create for the static website."
     type        = string
     default     = "some-example-bucket-for-a-static-website"
+
+    validation {
+        condition = (
+        # Check length is between 3 and 63.
+        length(var.bucket_name) >= 3 &&
+        length(var.bucket_name) <= 63 &&
+
+        # Ensure the bucket name is all lowercase.
+        var.bucket_name == lower(var.bucket_name) &&
+
+        # Ensure the bucket name matches allowed characters and starts/ends with a letter or digit.
+        can(regex("^([a-z0-9])([a-z0-9.-]*)([a-z0-9])$", var.bucket_name)) &&
+
+        # Disallow consecutive periods.
+        !strcontains(var.bucket_name, "..") &&
+
+        # Disallow names that look like an IP address.
+        !can(regex("^(\\d+\\.){3}\\d+$", var.bucket_name))
+        )
+        error_message = "Invalid bucket name. It must be 3–63 characters long, all lowercase, start and end with a letter or digit, contain only letters, numbers, hyphens, and periods, not contain consecutive periods, and not be an IP address."
+    }
 }
